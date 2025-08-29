3ceb1f87b2c617520f6b250293ab01a90b641c6b
meetbrahmbhatt91 <meetbrahmbhatt91@gmail.com>
2025-08-29 14:25:02 +0530

    Initial project scaffold with Xcode setup.

---
+++ PulseAI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PulseAI
+//
+//  Created by Meet Brahmbhatt on 29/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
