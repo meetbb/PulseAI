ebdab673392cd4247965c4c20e80e7f6a783f7af
meetbrahmbhatt91 <meetbrahmbhatt91@gmail.com>
2025-08-31 16:30:18 +0530

    Updated pre-commit,added KeyboardResponder and GeometryReader in LoginView.

---
+++ PulseAI/Presentation/Utils/KeyboardResponder.swift
@@ -0,0 +1,36 @@
+//
+//  KeyboardResponder.swift
+//  PulseAI
+//
+//  Created by Meet Brahmbhatt on 30/08/25.
+//
+import SwiftUI
+import Combine
+
+final class KeyboardResponder: ObservableObject {
+
+    @Published var currentHeight: CGFloat = 0
+    private var cancellableSet: Set<AnyCancellable> = []
+
+    init() {
+        let willShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .map { (notification) -> CGFloat in
+                (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect)?.height ?? 0
+            }
+
+        let willHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ -> CGFloat in 0 }
+
+        Publishers.Merge(willShow, willHide)
+            .receive(on: RunLoop.main)
+            .assign(to: \.currentHeight, on: self)
+            .store(in: &cancellableSet)
+    }
+}
+
+// MARK: - Hide keyboard helper
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
